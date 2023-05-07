20efbd1c3029097d0f81ee24977680057d1c4b53
Bhavani-5122004 <bhavani.chalasani@students.iiit.ac.in>
2023-05-07 14:58:28 +0530

    added

---
+++ temp/ordint.js
@@ -0,0 +1,8 @@
+function togglePage() {
+    if (window.location.href.endsWith('/ordint.html')) {
+      window.location.href = 'vieword.html';
+    } else {
+      window.location.href = 'ordint.html';
+    }
+  }
+  
