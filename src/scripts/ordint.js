059375367ef75fef08d6ccd6ebc2d38d719df43a
Jerseys <dev.faisal2004@gmail.com>
2023-05-07 17:15:03 +0530

    added slideswitch order page

--- src/scripts/ordint.js
+++ src/scripts/ordint.js
@@ -1,6 +1,6 @@
 function togglePage() {
     if (window.location.href.endsWith('/ordint.html')) {
-      window.location.href = 'vieword.html';
+      window.location.href = 'vieworder.html';
     } else {
       window.location.href = 'ordint.html';
     }
